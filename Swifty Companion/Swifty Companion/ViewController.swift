6b444d65e150f2d38f4f61c86769b60db6dff1b5
Felix Ntokozo THWALA <fthwala@e4r12p1.wethinkcode.co.za>
2018-10-16 16:19:05 +0200

    Swifty Companion

---
+++ Swifty Companion/Swifty Companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifty Companion
+//
+//  Created by Felix Ntokozo THWALA on 2018/10/16.
+//  Copyright © 2018 Felix Ntokozo THWALA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
